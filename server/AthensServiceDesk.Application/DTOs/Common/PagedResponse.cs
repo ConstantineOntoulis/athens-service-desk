ed978190f812e5cb17bcd65ad00aee8b6f124c47
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-06-07 18:48:24 +0300

    docs: update project status after DTO setup

--- server/AthensServiceDesk.Application/DTOs/Common/PagedResponse.cs
+++ server/AthensServiceDesk.Application/DTOs/Common/PagedResponse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AthensServiceDesk.Application.DTOs.Common;
+﻿namespace AthensServiceDesk.Application.DTOs.Common;
 
 public class PagedResponse<T>
 {
